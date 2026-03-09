8f05f5f1d591a2f8522d787fef0360dc701e8dad
NewDemo06032026 <alok2026@yahoo.com>
2026-03-09 12:04:41 +0530

    Added Terraform fmt and validate to pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "alok-terraform-rg"
+    resource_group_name  = "rg-devops-storage-lab"
     storage_account_name = "alokterraformstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
@@ -16,35 +16,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-devops-storage-lab"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "alokterraformstate"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
 }
