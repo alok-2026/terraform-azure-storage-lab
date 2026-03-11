fb99705d0485e85690211b6e2d8cb91c06b84241
NewDemo06032026 <alok2026@yahoo.com>
2026-03-11 23:00:40 +0530

    Fix NSG rule configuration

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -18,14 +18,16 @@ resource "azurerm_subnet" "subnet_private_endpoint" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
 resource "azurerm_network_security_group" "app_nsg" {
   name                = "nsg-app-subnet"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+
 resource "azurerm_network_security_rule" "allow_https" {
   name                        = "Allow-HTTPS"
-  priority                    = 101
+  priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
@@ -33,10 +35,10 @@ resource "azurerm_network_security_rule" "allow_https" {
   destination_port_range      = "443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-
   resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.app_nsg.name
 }
+
 resource "azurerm_subnet_network_security_group_association" "app_subnet_assoc" {
   subnet_id                 = azurerm_subnet.subnet_app.id
   network_security_group_id = azurerm_network_security_group.app_nsg.id
