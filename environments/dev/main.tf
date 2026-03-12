e74fd03046b02804554fb138c02cad8005941f0b
NewDemo06032026 <alok2026@yahoo.com>
2026-03-12 18:31:52 +0530

    Use existing resource group instead of creating it

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,21 +1,20 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
 module "network" {
   source = "../../modules/network"
 
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   vnet_name           = var.vnet_name
 }
 
 module "storage_account" {
   source = "../../modules/storage-account"
 
-  resource_group_name  = azurerm_resource_group.rg.name
-  location             = var.location
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  location             = data.azurerm_resource_group.rg.location
   storage_account_name = var.storage_account_name
   container_name       = var.container_name
 }
