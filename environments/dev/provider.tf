9eee7aacdd8ac75dbde10f3bd6efbba7578a6202
NewDemo06032026 <alok2026@yahoo.com>
2026-03-13 16:22:44 +0530

    Configure Azure remote backend for Terraform state

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-backend"
+    storage_account_name = "tfstatealok2026"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
