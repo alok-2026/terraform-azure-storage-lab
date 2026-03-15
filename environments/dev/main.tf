72e30b82ac01f833315f17a037be68c1f0991a79
NewDemo06032026 <alok2026@yahoo.com>
2026-03-15 18:58:22 +0530

    Add private endpoint for storage account

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,11 +1,11 @@
 module "network" {
   source = "../../modules/network"
 
-  vnet_name           = "dev-vnet"
-  resource_group_name = "rg-devops-storage-lab"
-  location            = "Central India"
+  vnet_name           = var.vnet_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
-  subnet_name   = "dev-subnet"
+  subnet_name   = "subnet-app"
   address_space = ["10.0.0.0/16"]
   subnet_prefix = ["10.0.1.0/24"]
 }
@@ -13,19 +13,20 @@ module "network" {
 module "storage_account" {
   source = "../../modules/storage-account"
 
-  storage_account_name = "alokdevstorage123"
-  resource_group_name  = "rg-devops-storage-lab"
-  location             = "Central India"
+  storage_account_name = var.storage_account_name
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  container_name       = var.container_name
 
-  container_name = "dev-container"
+  private_endpoint_subnet_id = module.network.subnet_private_endpoint_id
 }
-module "key_vault" {
 
+module "key_vault" {
   source = "../../modules/key-vault"
 
   key_vault_name      = "alokdev-keyvault"
-  location            = "Central India"
-  resource_group_name = "rg-devops-storage-lab"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   tenant_id = var.tenant_id
   object_id = var.object_id
