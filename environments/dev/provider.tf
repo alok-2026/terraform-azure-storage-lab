cbd459fcaf2e606ce2871d9dd293d446fa1438f5
NewDemo06032026 <alok2026@yahoo.com>
2026-03-16 16:07:00 +0530

    Fix Terraform backend storage account name to tfstatealokbackend

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-backend"
-    storage_account_name = "tfstatealok2026"
+    storage_account_name = "tfstatealokbackend"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
