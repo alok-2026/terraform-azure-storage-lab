5a221aef2ce0a412aa97caa2266f9963b2bbd908
NewDemo06032026 <alok2026@yahoo.com>
2026-03-16 22:39:34 +0530

    Terraform Azure lab completed with network, storage, private endpoint and key vault modules

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,33 +1,32 @@
 module "network" {
   source = "../../modules/network"
 
-  vnet_name           = var.vnet_name
   resource_group_name = var.resource_group_name
   location            = var.location
+  vnet_name           = var.vnet_name
+
+  address_space = ["10.0.0.0/16"]
 
   subnet_name   = "subnet-app"
-  address_space = ["10.0.0.0/16"]
   subnet_prefix = ["10.0.1.0/24"]
 }
 
 module "storage_account" {
   source = "../../modules/storage-account"
 
-  storage_account_name = var.storage_account_name
   resource_group_name  = var.resource_group_name
   location             = var.location
+  storage_account_name = var.storage_account_name
   container_name       = var.container_name
 
   private_endpoint_subnet_id = module.network.subnet_private_endpoint_id
 }
 
-module "key_vault" {
-  source = "../../modules/key-vault"
+module "keyvault" {
+  source = "../../modules/keyvault"
 
-  key_vault_name      = "alokdev-keyvault"
-  location            = var.location
   resource_group_name = var.resource_group_name
-
-  tenant_id = var.tenant_id
-  object_id = var.object_id
+  location            = var.location
+  tenant_id           = var.tenant_id
+  object_id           = var.object_id
 }
